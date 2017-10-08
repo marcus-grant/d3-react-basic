dc53e92c64042873665cbd9cbedd2daaacf63236
Marcus Grant <marcusfg@gmail.com>
2017-10-07 22:29:23 -0400

    Add Routes & Semantic UI

--- imports/app.jsx
+++ imports/app.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Chart from './chart.jsx';
+import Home from './home.jsx';
+
+const routes = [
+  {
+    path: '/',
+    text: 'HOME',
+    exact: true,
+    component: <Home />,
+  },
+  {
+    path: '/scatter',
+    text: 'SCATTER',
+    component: <Chart type='scatter'/>
+  },
+];
 
 export default class App extends React.Component {
+  renderRoutes() {
+    return ({
+      routes.map((route, index) => (
+        <Route
+          path={route.path},
+          component={route.component},
+          exact={route.exact},
+        />
+      ));
+    });
+  }
   render() {
     return (
       <div>
-        <h1>D3 Randomized Scatter Plot</h1>
-        <Chart />
+        <BrowserRouter>
+          {renderRoutes()}
+        <BrowserRouter/>
       </div>
     );
   }
