ff4864cc245077b32aa17226c19c70d24ca82439
Marcus Grant <marcusfg@gmail.com>
2017-10-18 14:46:55 -0400

    install d3v4, now migrating ccomponents

--- imports/components/d3/axis.jsx
+++ imports/components/d3/axis.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import d3    from 'd3';
+import d3 from 'd3';
 
 export default class Axis extends React.Component {
   componentDidMount() {
@@ -11,13 +11,13 @@ export default class Axis extends React.Component {
   }
 
   renderAxis() {
-    var node  = this.refs.axis;
-    var axis = d3.svg.axis().orient(this.props.orient).ticks(5).scale(this.props.scale);
+    const node = this.refs.axis;
+    const axis = d3.svg.axis().orient(this.props.orient).ticks(5).scale(this.props.scale);
     d3.select(node).call(axis);
   }
 
   render() {
-    return <g className="axis" ref="axis" transform={this.props.translate}></g>
+    return <g className="axis" ref="axis" transform={this.props.translate} />;
   }
 }
 
