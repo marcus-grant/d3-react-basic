a2632a9fe943750367ef3a9cb1e1a9113db0714d
Marcus Grant <marcusfg@gmail.com>
2017-10-17 21:03:34 -0400

    now functional and functioning

--- imports/components/chart-controls.jsx
+++ imports/components/chart-controls.jsx
@@ -1,37 +1,73 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class ChartControls extends React.Component {
-  render() {
-    const radiusControls = (
-      <div className="chart-controls__container">
-        <h6>Radius</h6>
-        <button
-          className="chart-controls__button"
-          onClick={() => this.decrementRadius()}
-        >
-        -
-        </button>
-        <button
-          className="chart-controls__button"
-          onClick={() => this.incrementRadius()}
-        >
-        +
-        </button>
-      </div>
-    );
-    return (
-      <div className="chart-controls">
-        <div className="chart-controls__container">
-          <p>Data</p>
-          <button
-            className="chart-controls__button"
-            onClick={() => this.randomizeData()}
-          >
-            Randomize
-          </button>
-        </div>
-        { radiusControls }
-      </div>
-    );
-  }
-}
+const plusMinusControls = (title, plusCallback, minusCallback) => (
+  <div className="chart-controls__container">
+    <h6>{title}</h6>
+    <button
+      className="chart-controls__button"
+      onClick={minusCallback}
+    >
+    -
+    </button>
+    <button
+      className="chart-controls__button"
+      onClick={plusCallback}
+    >
+    +
+    </button>
+  </div>
+);
+
+
+const radiusControls = (plusCallback, minusCallback) =>
+  plusMinusControls('Radius', plusCallback, minusCallback);
+
+const singleControl = (title, actionTitle, callback) => (
+  <div className="chart-controls__container">
+    <h6>{title}</h6>
+    <button
+      className="chart-controls__button"
+      onClick={callback}
+    >{actionTitle}
+    </button>
+  </div>
+);
+
+const randomizeControls = callback =>
+  singleControl('Data', 'Randomize', callback);
+
+const ChartControls = props => (
+  <div className="chart-controls">
+    { randomizeControls(props.randomizeDataCallback) }
+    {
+      radiusControls(
+        props.incrementRadiusCallback,
+        props.decrementRadiusCallback,
+      )
+    };
+  </div>
+);
+
+export default ChartControls;
+
+// export default class ChartControls extends React.Component {
+//   render() {
+//     console.log(this.props);
+//     return (
+//       <div className="chart-controls">
+//         { randomizeControls(this.props.randomizeDataCallback) }
+//         { radiusControls(
+//           this.props.incrementRadiusCallback,
+//           this.props.decrementRadiusCallback,
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+ChartControls.propTypes = {
+  randomizeDataCallback: PropTypes.func.isRequired,
+  incrementRadiusCallback: PropTypes.func.isRequired,
+  decrementRadiusCallback: PropTypes.func.isRequired,
+};
