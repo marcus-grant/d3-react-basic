bd242f9d0414d8d9ce54f71ab232f0229439cb79
Marcus Grant <marcusfg@gmail.com>
2017-10-17 21:05:04 -0400

    partial fix to fit linter

--- imports/components/d3/data-dots.jsx
+++ imports/components/d3/data-dots.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const renderDots = (props) => {
-  return (coords, index) => {
-    const dotProps = {
-      cx:   props.xScale(coords[0]),
-      cy:   props.yScale(coords[1]),
-      r:    props.radius || 2 ,
-      key:  index
-    };
-    return <circle {...dotProps} />;
+const renderDots = props => (coords, index) => {
+  const dotProps = {
+    cx: props.xScale(coords[0]),
+    cy: props.yScale(coords[1]),
+    r: props.radius || 2,
+    key: index,
   };
+  return <circle {...dotProps} />;
 };
 
-export default (props) => { 
-  return <g>{ props.data.map(renderDots(props)) }</g>;
+const DataDots = props => (
+  <g>{ props.data.map(renderDots(props)) }</g>
+);
+
+export default DataDots;
+
+DataDots.propTypes = {
+  xScale: PropTypes.func.isRequired,
 };
